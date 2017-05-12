e8df1bb981f163d29816908d4414870b308e0e01
Herman Barnardt <hbarnardt@retrorabbit.co.za>
2017-05-12 15:42:21 +0200

    Added more responses

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 module.exports = function(bp) {
   bp.middlewares.load()
 
-  bp.hear('hello', event => { // Capture messages that are 'hello'
-    bp.messenger.sendText(event.user.id, 'Hello, human!') // Respond to the user with 'Hello, human!'
+  bp.hear({ platform: 'slack', type: 'message', text: 'Hello' }, (event, next) => {
+    bp.slack.sendText(event.channel.id, 'Hi')
   })
 
-  bp.hear({ platform: 'slack', type: 'message', text: 'Hello' }, (event, next) => {
-    bp.slack.sendText(event.channel.id, 'Welcome on Botpress!!!')
-  })
+  bp.hear({ platform: 'slack', text:'I love you'}, event => {
+    bp.slack.sendReaction('kissing_smiling_eyes', { channel: event.channel.id, timestamp: event.ts })
+  }) 
+
+  bp.hear({ platform: 'slack', text:'Who are you?'}, event => {
+    bp.slack.sendReaction('I am Marvin, the Retro Rabbit', { channel: event.channel.id, timestamp: event.ts })
+  }) 
 }
