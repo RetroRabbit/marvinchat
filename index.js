43a19a8912cedf8639ccc9a7fd065fea450334c9
Herman Barnardt <hbarnardt@retrorabbit.co.za>
2017-05-12 15:30:29 +0200

    Updated index.js to listen on slack

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ module.exports = function(bp) {
   bp.hear('hello', event => { // Capture messages that are 'hello'
     bp.messenger.sendText(event.user.id, 'Hello, human!') // Respond to the user with 'Hello, human!'
   })
+
+  bp.hear({ platform: 'slack', type: 'message', text: 'Hello' }, (event, next) => {
+    bp.slack.sendText(event.channel.id, 'Welcome on Botpress!!!')
+  })
 }
